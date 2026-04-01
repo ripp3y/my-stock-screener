7e2972d69c194625a7b1f9d62f55412c2aba25ed
ripp3y <ripp3y1973@gmail.com>
2026-03-31 22:39:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,8 @@ info = ticker_obj.info
 
 # 3. Now the metric will work because 'info' exists
 st.metric("Forward PE", f"{info.get('forwardPE', 'N/A')}")
+# Get the value, default to 0 if missing, and round to 1 decimal place
+f_pe = round(info.get('forwardPE', 0), 1)
+
+# Display the polished metric
+st.metric("Forward PE", f"{f_pe}")
